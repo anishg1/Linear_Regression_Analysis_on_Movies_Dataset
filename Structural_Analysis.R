3747a91db965c4c21163784c02c5f2f0287cd373
Anish Ganguli <40606822+anishg1@users.noreply.github.com>
2021-03-07 12:52:08 +0530

    Update Structural_Analysis.R

--- Structural_Analysis.R
+++ Structural_Analysis.R
@@ -9,6 +9,13 @@ library(readxl)
 library(data.table)
 library(stringr)
 
+## Required function for creating proper case
+
+toproper <- function(x){
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
+
 ## Reading the Movie Credit Datasets and appending them
 
 pattern1 = list.files(pattern = "xlsx")
@@ -32,6 +39,15 @@ rm(movie_credits_part_1,movie_credits_part_2,extra_obs,data_name)
 
 movie_data = fread("tmdb_5000_movies.csv",stringsAsFactors = F) %>% as.data.frame()
 
+## Reading the ISO 639 -1 language codes
+
+language_code = read_excel("Language.xlsx",sheet = 1)
+language_code = language_code[,c("ISO639-1Code","LanguageName")]
+language_code = subset(language_code, is.na(language_code$`ISO639-1Code`) != TRUE)
+names(language_code)[1] = "original_language"
+
+## Fixing column names for the master dataset
+
 genre_cols = c("id","name")
 cast_cols = c("cast_id","character","credit_id","gender","id","name","order")
 crew_cols = c("credit_id","department","gender","id","job","name")
@@ -44,9 +60,11 @@ prod_coun_cols = c("iso_3166_1","name")
 
 ## Functions for restructuring the columns
 
-split_vals = function(data_input,var,n,col_names){
+split_vals = function(data_input,var,col_names){
   list_var1 = NULL
   e1 = NULL
+  
+  n = length(col_names)
   
   for(i in 1:(length(data_input[,var]))){
     
@@ -107,60 +125,57 @@ split_vals = function(data_input,var,n,col_names){
     e1[,1] = NULL
   }
   
+  rownames(list_var1) = NULL
+  e1 = distinct(e1)
+  
+  names(list_var1) = paste0(toproper(var),"-",names(list_var1))
+  
+  fwrite(e1,paste0(toproper(var),"_Master_Data_",Sys.Date(),".csv"))
+  
+  
   final_list = list(list_var1,e1)
   return(final_list) 
 }
 
 ## Structured columns in a data frame
 
-genres = split_vals(movie_data,"genres",2,genre_cols)[[1]]
-cast = split_vals(movie_credits,"cast",7,cast_cols)[[1]]
-crew = split_vals(movie_credits,"crew",6,crew_cols)[[1]]
-prod_comp = split_vals(movie_data,"production_companies",2,prod_comp_cols)[[1]]
-prod_country = split_vals(movie_data,"production_countries",2,prod_coun_cols)[[1]]
-rownames(genres) = NULL
-rownames(cast) = NULL
-rownames(crew) = NULL
-rownames(prod_comp) = NULL
-rownames(prod_country) = NULL
+genres = split_vals(movie_data,"genres",genre_cols)[[1]]
+cast = split_vals(movie_credits,"cast",cast_cols)[[1]]
+crew = split_vals(movie_credits,"crew",crew_cols)[[1]]
+prod_comp = split_vals(movie_data,"production_companies",prod_comp_cols)[[1]]
+prod_country = split_vals(movie_data,"production_countries",prod_coun_cols)[[1]]
 
 ## Master data based on Cast_ID, Crew_ID, Genre_ID
 
-genres_level = distinct(split_vals(movie_data,"genres",2,genre_cols)[[2]])
-cast_level = distinct(split_vals(movie_credits,"cast",7,cast_cols)[[2]])
-crew_level = distinct(split_vals(movie_credits,"crew",6,crew_cols)[[2]])
-prod_comp_level = distinct(split_vals(movie_data,"production_companies",2,prod_comp_cols)[[2]])
-prod_country_level = distinct(split_vals(movie_data,"production_countries",2,prod_coun_cols)[[2]])
+genres_level = split_vals(movie_data,"genres",genre_cols)[[2]]
+cast_level = split_vals(movie_credits,"cast",cast_cols)[[2]]
+crew_level = split_vals(movie_credits,"crew",crew_cols)[[2]]
+prod_comp_level = split_vals(movie_data,"production_companies",prod_comp_cols)[[2]]
+prod_country_level = split_vals(movie_data,"production_countries",prod_coun_cols)[[2]]
 
-## Fixing the names of the structured columns
+## Fixing the language column in Movie Data
 
-names(genres) = c("genres","genre_id")
-names(cast) = c("order","cast_name","id","cast_gender","credit_id","character","cast_id")
-names(crew)[c(1,3,4)] = c("crew_name","crew_id","crew_gender")
-names(prod_comp) = c("prod_company_id","prod_company_name")
-names(prod_country)[1] = "prod_country_name"
+movie_data = movie_data %>% left_join(language_code,by="original_language")
+movie_data$LanguageName[movie_data$original_language == "cn"] = "Chinese"
+movie_data$LanguageName[movie_data$original_language == "xx"] = "Invented Language"
 
 ## Final structured Movie and Credits dataset
 
 movie_data_structured = cbind(movie_data,genres,prod_comp,prod_country) %>% as.data.frame()
-movie_data_structured = movie_data_structured %>% dplyr::select(-genres,-keywords,-overview,-production_companies,-production_countries,-spoken_languages)
+movie_data_structured = movie_data_structured %>% dplyr::select(-genres,-keywords,-overview,-production_companies,-production_countries,-original_language,-spoken_language)
 names(movie_data_structured)[3] = "movie_id"
 
 ######################
 
 movie_credits_structured = cbind(movie_credits,cast,crew) %>% as.data.frame()
-movie_credits_structured = movie_credits_structured %>% dplyr::select(-crew,-cast,-order,-credit_id)
+movie_credits_structured = movie_credits_structured %>% dplyr::select(-crew,-cast)
 
 ## Clearing up the environment
 
 rm(cast,cast_cols,crew,crew_cols,genres,genre_cols,prod_comp,prod_comp_cols,prod_country,prod_coun_cols)
 
-## Exporting the structured dataframes and master data to the working directory
+## Exporting the structured dataframes and language data to the working directory
 
-fwrite(movie_data_structured,"Movie_Data_Structured_03Mar_2021.csv")
-fwrite(movie_credits_structured,"Movie_Credits_Structured_03Mar_2021.csv")
-fwrite(genres_level,"Genre_Master_03Mar_2021.csv")
-fwrite(cast_level,"Cast_Master_03Mar_2021.csv")
-fwrite(crew_level,"Crew_Master_03Mar_2021.csv")
-fwrite(prod_comp_level,"Production_Comapanies_Master_03Mar_2021.csv")
-fwrite(prod_country_level,"Production_Countries_Master_03Mar_2021.csv")
+fwrite(movie_data_structured,paste0("Movie_Data_structured_",Sys.Date(),".csv"))
+fwrite(movie_credits_structured,paste0("Movie_Credits_structured_",Sys.Date(),".csv"))
+fwrite(language_code,paste0("Language_ISO639-1_Code_",Sys.Date(),".csv"))
